12fbc09cd3b312fc0274bb55f9726ce8300c5c19
Maximilian Walker <maximilian.walker@gmx.de>
2025-06-26 23:35:19 +0200

    tutorial #7 to #9

--- src/main/java/net/straded/tutorialmod/TutorialMod.java
+++ src/main/java/net/straded/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.straded.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.straded.tutorialmod.block.ModBlocks;
 import net.straded.tutorialmod.item.ModItemGroups;
 import net.straded.tutorialmod.item.ModItems;
@@ -18,5 +19,7 @@ public class TutorialMod implements ModInitializer {
 
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
+
+		FuelRegistry.INSTANCE.add(ModItems.STARLIGHT_ASHES, 600);
 	}
 }
