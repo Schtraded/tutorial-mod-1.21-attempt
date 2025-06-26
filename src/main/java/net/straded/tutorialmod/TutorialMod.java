a09f2e8d16ab8b259bceea6e50b4cb43019c9f6a
Maximilian Walker <maximilian.walker@gmx.de>
2025-06-26 19:00:30 +0200

    tutorial #5

--- src/main/java/net/straded/tutorialmod/TutorialMod.java
+++ src/main/java/net/straded/tutorialmod/TutorialMod.java
@@ -2,6 +2,8 @@ package net.straded.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.straded.tutorialmod.block.ModBlocks;
+import net.straded.tutorialmod.item.ModItemGroups;
 import net.straded.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +14,9 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItemGroups.registerItemGroups();
+
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
