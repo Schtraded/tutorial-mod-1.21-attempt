e0efda9df55bba9e4874b390cafcc00b003444d4
Maximilian Walker <maximilian.walker@gmx.de>
2025-07-10 13:48:10 +0200

    Tutorial #12/#13

--- src/main/java/net/straded/tutorialmod/TutorialModClient.java
+++ src/main/java/net/straded/tutorialmod/TutorialModClient.java
@@ -1,10 +1,15 @@
 package net.straded.tutorialmod;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
+import net.straded.tutorialmod.block.ModBlocks;
 
 public class TutorialModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.PINK_GARNET_DOOR, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.PINK_GARNET_TRAPDOOR, RenderLayer.getCutout());
 
     }
 }
